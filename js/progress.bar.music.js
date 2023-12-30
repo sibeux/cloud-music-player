20ea95c1356f011d8ac54693c2866425da786a8c
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-12-30 22:33:18 +0700

    Refactor favorite button functionality

--- js/progress.bar.music.js
+++ js/progress.bar.music.js
@@ -1,16 +1,6 @@
 const playIcon = document.getElementById("play-icon");
 let statePlay = "play";
 
-const favoriteIcon = document.getElementById("favorite-icon");
-// get style color of favorite icon
-const favoriteIconStyle = window.getComputedStyle(favoriteIcon);
-// get color of favorite icon
-const favoriteIconColor = favoriteIconStyle.getPropertyValue("color");
-let stateFavorite = "favorite";
-if (favoriteIconColor === "rgb(255, 255, 255)") {
-	stateFavorite = "unfavorite";
-}
-
 // Play and Pause
 playIcon.addEventListener("click", () => {
 	playIcon.classList.remove("fa-play");
@@ -23,16 +13,27 @@ playIcon.addEventListener("click", () => {
 	}
 });
 
-// Favorite
-favoriteIcon.addEventListener("click", () => {
-	favoriteIcon.classList.remove("fa-heart");
+function changeFavoriteButton(id) {
+	const favoriteIcon = document.getElementsByClassName("fa-heart");
+
+	const button = favoriteIcon[id];
+
+	// get style color of favorite icon
+	const favoriteIconStyle = window.getComputedStyle(button);
+	// get color of favorite icon
+	const favoriteIconColor = favoriteIconStyle.getPropertyValue("color");
+	let stateFavorite = "favorite";
+	if (favoriteIconColor === "rgb(255, 255, 255)") {
+		stateFavorite = "unfavorite";
+	}
+
+	// Favorite and Unfavorite function
+	button.classList.remove("fa-heart");
 	if (stateFavorite === "favorite") {
-		favoriteIcon.innerHTML =
-			'<i class="far fa-heart" style="color: #fff;"></i>';
+		button.innerHTML = '<i class="far fa-heart" style="color: #fff;"></i>';
 		stateFavorite = "unfavorite";
 	} else {
-		favoriteIcon.innerHTML =
-			'<i class="fas fa-heart" style="color: #1fd660"></i>';
+		button.innerHTML = '<i class="fas fa-heart" style="color: #1fd660"></i>';
 		stateFavorite = "favorite";
 	}
-});
+}
