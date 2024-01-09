b73cb0f4497a0f290b8b38e7279d1d471cd18a58
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-09 23:28:38 +0700

    Update database credentials

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', 'localhost');
-define('SIBEUX', 'root');
-define('pass', '');
+define('SIBEUX', 'sibk1922');
+define('pass', '1NvgEHFnwvDN96');
 define('DB', 'sibk1922_cloud_music');
 $db = new mysqli(HOST, SIBEUX, pass, DB);
 
