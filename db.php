630f4efdb78098a0cb1db3074a64d797ca6f0d82
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-12 23:03:19 +0700

    Update database connection in db.php and index.php

--- db.php
+++ db.php
@@ -1,11 +1,2 @@
 <?php
 
-define('HOST', 'localhost');
-define('SIBEUX', 'sibk1922');
-define('pass', '1NvgEHFnwvDN96');
-define('DB', 'sibk1922_cloud_music');
-$db = new mysqli(HOST, SIBEUX, pass, DB);
-
-if ($db->connect_errno) {
-    die('Tidak dapat terhubung ke database');
-}
