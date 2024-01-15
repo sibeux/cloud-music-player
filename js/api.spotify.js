335e89c488e8706649620843afe2d83fe6f834db
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-16 02:43:10 +0700

    Update import statement for getDataTimeFileMusic function

--- js/api.spotify.js
+++ js/api.spotify.js
@@ -83,12 +83,3 @@ export function getDataFromAPISpotify(link, id) {
 			console.error("Error refreshing access token:", error);
 		});
 }
-
-// get data time from music file
-export function getDataTimeFileMusic(link, id) {
-	const timeArray = document.getElementsByClassName("time_music");
-	var x = link;
-	console.log(x);
-
-	timeArray[id].innerHTML = x;
-}
