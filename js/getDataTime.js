ca01769f576091ad0d575b1382c089dad8eca32c
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-16 03:05:51 +0700

    Refactor getDataTimeFileMusic function to calculate and display the duration of the audio file

--- js/getDataTime.js
+++ js/getDataTime.js
@@ -1,8 +1,16 @@
 // get data time from music file
 export function getDataTimeFileMusic(link, id) {
 	const timeArray = document.getElementsByClassName("time_music");
-	var x = link.duration;
-	console.log(x);
+	const audioContext = new AudioContext();
 
-	timeArray[id].innerHTML = x;
+	// Load the audio file
+	fetch(link)
+		.then((response) => response.arrayBuffer())
+		.then((buffer) => audioContext.decodeAudioData(buffer))
+		.then((audioBuffer) => {
+			// Get the duration of the audio file
+			const duration = audioBuffer.duration;
+			console.log(`The audio file duration is ${duration} seconds`);
+			timeArray[id].innerHTML = duration;
+		}); 
 }
