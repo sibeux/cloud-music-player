e6b056ee1cb30f64c96cefc15d06922cb56e75bf
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-16 02:37:49 +0700

    Add console log to display duration

--- js/api.spotify.js
+++ js/api.spotify.js
@@ -88,6 +88,7 @@ export function getDataFromAPISpotify(link, id) {
 export function getDataTimeFileMusic(link, id) {
 	const timeArray = document.getElementsByClassName("time_music");
 	var x = link.duration;
+	console.log(x);
 
 	timeArray[id].innerHTML = x;
 }
