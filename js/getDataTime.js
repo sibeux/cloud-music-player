10d43e74474d4b8d6093b3509bbef0708c0efc89
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-16 03:14:19 +0700

    Add console log to display duration

--- js/getDataTime.js
+++ js/getDataTime.js
@@ -5,5 +5,6 @@ export function getDataTimeFileMusic(link, id) {
 
 	const timeArray = document.getElementsByClassName("time_music");
 	var x = document.getElementById("player_music").duration;
+	console.log(x);
 	timeArray[id].innerHTML = x;
 }
