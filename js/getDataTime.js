0ae799e02c3e0f9bf774af8c0702e204b54c2664
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-16 02:47:06 +0700

    Refactor variable assignment in getDataTimeFileMusic function

--- js/getDataTime.js
+++ js/getDataTime.js
@@ -1,7 +1,7 @@
 // get data time from music file
 export function getDataTimeFileMusic(link, id) {
 	const timeArray = document.getElementsByClassName("time_music");
-	var x = link.duration;
+	var x = timeArray[id].duration;
 	console.log(x);
 
 	timeArray[id].innerHTML = x;
