9cd289ea59b68aba0cdfc4c9f33731b3d0dbe969
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-01-16 02:47:59 +0700

    Fix duration retrieval in getDataTimeFileMusic function

--- js/getDataTime.js
+++ js/getDataTime.js
@@ -1,7 +1,7 @@
 // get data time from music file
 export function getDataTimeFileMusic(link, id) {
 	const timeArray = document.getElementsByClassName("time_music");
-	var x = timeArray[id].duration;
+	var x = timeArray[id];
 	console.log(x);
 
 	timeArray[id].innerHTML = x;
