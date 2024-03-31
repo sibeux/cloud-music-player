57fc2d1551964d07e6f3987b7725bd310abc2512
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-04-01 01:53:40 +0700

    Update database connection and add error handling

--- database/mobile-music-player/api/db.php
+++ database/mobile-music-player/api/db.php
@@ -1,6 +1,15 @@
 <?php
 
-include './db.php';
+define('HOST', 'localhost');
+define('SIBEUX', 'sibk1922');
+define('pass', '1NvgEHFnwvDN96');
+define('DB', 'sibk1922_cloud_music');
+
+$db = new mysqli(HOST, SIBEUX, pass, DB);
+
+if ($db->connect_errno) {
+    die('Tidak dapat terhubung ke database');
+}
 
 $query = $db->query("SELECT * FROM music");
 $result = array();
