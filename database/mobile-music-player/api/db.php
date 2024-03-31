321f59f849a7ae653a6243cc2d6898bc3d365034
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-04-01 04:55:40 +0700

    Q

--- database/mobile-music-player/api/db.php
+++ database/mobile-music-player/api/db.php
@@ -16,8 +16,7 @@ $result = array();
 
 while ($row = mysqli_fetch_assoc($query)) {
     // add each row returned into an array
-    echo json_encode($row);
-    $result[] = $row;
+    $result[] = json_encode($row);
 }
 
 echo json_encode($result);
