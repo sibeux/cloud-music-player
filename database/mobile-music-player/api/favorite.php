0f7547b1d94409a7aa9f4cd3871b980f5a0d8cbd
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-04-30 23:50:33 +0700

    Add favorite.php API endpoint for updating favorite status in the database

---
+++ database/mobile-music-player/api/favorite.php
@@ -0,0 +1,31 @@
+<?php
+
+define('HOST', 'localhost');
+define('SIBEUX', 'sibk1922');
+define('pass', '1NvgEHFnwvDN96');
+define('DB', 'sibk1922_cloud_music');
+
+$conn = new mysqli(HOST, SIBEUX, pass, DB);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "";
+
+if (isset($_GET['_favorite']) && isset($_GET['_id'])) {
+
+    $_favorite = $_GET['_favorite'];
+    $_id = $_GET['_id'];
+
+    $sql = "UPDATE music SET favorite = '$_favorite' WHERE id = '$_id'";
+}
+
+// Query to retrieve data from MySQL
+$result = $conn->query($sql);
+
+// Check if the query was successful
+if (!$result) {
+    die("Query failed: " . $conn->error);
+}
