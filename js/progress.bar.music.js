fc62ba8e8fc7b6b85760602213cd70616e384c30
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-06-28 22:15:36 +0700

    ppp

--- js/progress.bar.music.js
+++ js/progress.bar.music.js
@@ -144,8 +144,8 @@ function nextMusic(countMusic) {
 		.then((response) => response.json())
 		.then((json) =>
 			animatedPlayMusic(
-				json[randomNumber]["id_music"] - 1,
-				json[randomNumber]["link"],
+				json[randomNumber]["index"] - 1,
+				json[randomNumber]["link_gdrive"],
 				countMusic
 			)
 		);
