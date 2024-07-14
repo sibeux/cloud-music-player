4f3be78444b2ee19f389cb9111961d971289c606
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-07-14 23:56:05 +0700

    asa

--- database/mobile-music-player/api/playlist.php
+++ database/mobile-music-player/api/playlist.php
@@ -6,7 +6,7 @@ $sql = "SELECT * FROM playlist ORDER BY pin";
 
 if (isset($_GET['sort']) && isset($_GET['filter'])) {
     $sort = $_GET['sort'];
-    $filter = $_GET['filter'] == '' ? '1=1' : $_GET['filter'];
+    $filter = $_GET['filter'] == '' ? null : $_GET['filter'];
 
     if ($sort == 'uid') {
         $sql = "SELECT * FROM playlist WHERE ('$filter' IS NULL OR type = '$filter') ORDER BY pin, date_pin asc, uid desc";
