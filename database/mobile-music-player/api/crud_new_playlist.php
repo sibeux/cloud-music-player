7a128f51bf169ee1d87e72d8ed226444d3156e22
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-08-19 20:14:25 +0700

    Refactor playlist.php to add support for retrieving playlists by type

--- database/mobile-music-player/api/crud_new_playlist.php
+++ database/mobile-music-player/api/crud_new_playlist.php
@@ -3,6 +3,7 @@
 include './connection.php';
 
 $sql = "";
+$sql2 = "";
 
 if (isset($_GET['action']) && isset($_GET['playlist_name']) && ($_GET['action'] == 'create')) {
 
@@ -10,10 +11,18 @@ if (isset($_GET['action']) && isset($_GET['playlist_name']) && ($_GET['action'] 
 
     $sql = "INSERT INTO playlist (uid, name, image, type, author, pin, date_pin, date, editable) 
     values (NULL, '$name', NULL, 'playlist', 'Nasrul Wahabi', 'false', NULL, NOW(), 'true')";
+} else if (
+    isset($_GET['action']) && isset($_GET['playlist_uid']) && ($_GET['action'] == 'delete')
+) {
+    $uid = $_GET['playlist_uid'];
+
+    $sql = "DELETE FROM playlist WHERE playlist.uid = '$uid'";
+    $sql2 = "DELETE FROM playlist_music WHERE playlist_music.id_playlist = '$uid'";
 }
 
 // Query to retrieve data from MySQL
 $result = $conn->query($sql);
+$result = $conn->query($sql2);
 
 // Check if the query was successful
 if (!$result) {
