951752092bced64d7ae0187a11c0601cb9e6fd43
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-09-08 06:43:45 +0700

    a

--- indopride.php
+++ indopride.php
@@ -26,9 +26,6 @@ $data = json_decode($response, true);
 
 $api_key = $data[0]['gdrive_api'];
 
-// Tampilkan hasil
-// print_r($data);
-
 function checkUrlFromDrive(string $url_db, string $gdrive_api_key)
 {
     if (strpos($url_db, "drive.google.com") !== false) {
@@ -105,6 +102,7 @@ function checkUrlFromDrive(string $url_db, string $gdrive_api_key)
                                                             join playlist on music.category like playlist.uid
                                                             WHERE music.category = '1'
                                                             ORDER BY music.title ASC";
+                                                            
                                             $result_music = $db->query($sql_music);
 
                                             $count_music = mysqli_num_rows($result_music);
@@ -122,42 +120,25 @@ function checkUrlFromDrive(string $url_db, string $gdrive_api_key)
                                                 $favorite = $array_data_music['favorite'];
                                                 $album = $array_data_music['album'];
                                                 $time = $array_data_music['time'];
-                                                $cover = $array_data_music['cover'];
+                                                $cover = checkUrlFromDrive($array_data_music['cover'], $api_key);
                                                 $date_added = $array_data_music['date_added'];
-
-                                                $musicData = json_encode($array_data_music);
 
                                                 if ($array_data_music['link_spotify'] == null) {
 
                                                     // cut string title if too long
-                                                    if (strlen($array_data_music['title']) > 25) {
-                                                        $title = substr($array_data_music['title'], 0, 25) . "...";
-                                                    } else {
-                                                        $title = $array_data_music['title'];
+                                                    if (strlen($title) > 25) {
+                                                        $title = substr($title, 0, 25) . "...";
                                                     }
 
                                                     // cut string artist if too long
-                                                    if (strlen($array_data_music['artist']) > 35) {
-                                                        $artist = substr($array_data_music['artist'], 0, 35) . "...";
-                                                    } else {
-                                                        $artist = $array_data_music['artist'];
+                                                    if (strlen($artist) > 35) {
+                                                        $artist = substr($artist, 0, 35) . "...";
                                                     }
 
                                                     // cut string album if too long
-                                                    if (strlen($array_data_music['album']) > 80) {
-                                                        $album = substr($array_data_music['album'], 0, 80) . "...";
-                                                    } else {
-                                                        $album = $array_data_music['album'];
+                                                    if (strlen($album) > 80) {
+                                                        $album = substr($album, 0, 80) . "...";
                                                     }
-
-                                                    $cover = checkUrlFromDrive($array_data_music['cover'], $api_key);
-                                                    $time = $array_data_music['time'];
-                                            
-                                                } else {
-                                                    echo "<script type='module'>
-                                                        import { getDataFromAPISpotify } from './js/api.spotify.js';
-                                                        getDataFromAPISpotify('{$array_data_music['link_spotify']}', {$number_music}-1, 'null');
-                                                    </script>";
                                                 }
                                                 ?>
                                             <ul class="album_inner_list_padding">
@@ -165,7 +146,7 @@ function checkUrlFromDrive(string $url_db, string $gdrive_api_key)
                                                             <?php echo $number_music; ?>
                                                         </span>
                                                         <span class="play_hover"
-                                                            onclick="animatedPlayMusic(<?php echo $number_music - 1 ?>,'<?php echo $link_drive ?>','<?php echo $count_music ?>', '<?php echo $id_music ?>', '<?php echo $musicData ?>')"><i
+                                                            onclick="animatedPlayMusic(<?php echo $number_music - 1 ?>,'<?php echo $link_drive ?>','<?php echo $count_music ?>', '<?php echo $id_music ?>')"><i
                                                                 class="flaticon-play-button"></i></span></a>
                                                 </li>
                                                 <li class="song_title_width">
