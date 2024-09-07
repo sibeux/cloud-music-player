0fcec5d56ef8a009ccb8bbe617417389a177a455
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-09-08 06:19:56 +0700

    song_title_width

--- indopride.php
+++ indopride.php
@@ -125,6 +125,19 @@ function checkUrlFromDrive(string $url_db, string $gdrive_api_key)
                                                 $cover = $array_data_music['cover'];
                                                 $date_added = $array_data_music['date_added'];
 
+                                                $musicData = json_encode(array(
+                                                    'id_music' => $id_music,
+                                                    'category' => $category,
+                                                    'title' => $title,
+                                                    'artist' => $artist,
+                                                    'favorite' => $favorite,
+                                                    'album' => $album,
+                                                    'time' => $time,
+                                                    'cover' => $cover,
+                                                    'link_gdrive' => $link_drive,
+                                                    'date_added' => $date_added
+                                                ));
+
                                                 if ($array_data_music['link_spotify'] == null) {
 
                                                     // cut string title if too long
@@ -163,7 +176,7 @@ function checkUrlFromDrive(string $url_db, string $gdrive_api_key)
                                                             <?php echo $number_music; ?>
                                                         </span>
                                                         <span class="play_hover"
-                                                            onclick="animatedPlayMusic(<?php echo $number_music - 1 ?>,'<?php echo $link_drive ?>','<?php echo $count_music ?>', '<?php echo $id_music ?>', '<?php echo json_encode($array_data_music) ?>')"><i
+                                                            onclick="animatedPlayMusic(<?php echo $number_music - 1 ?>,'<?php echo $link_drive ?>','<?php echo $count_music ?>', '<?php echo $id_music ?>', '<?php echo $musicData ?>')"><i
                                                                 class="flaticon-play-button"></i></span></a>
                                                 </li>
                                                 <li class="song_title_width">
