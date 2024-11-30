c6cd1ca70f624312fdba5c717a69a7f5f4601ac8
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-11-30 18:23:23 +0700

    Remove jQuery and Bootstrap scripts from alternative.php; refactor loadMoreMusic function in lazy.js to use XMLHttpRequest for data fetching and update appendMusic function to handle music rendering

--- js/lazy.js
+++ js/lazy.js
@@ -11,31 +11,32 @@ window.addEventListener('scroll', function () {
 });
 
 function loadMoreMusic(page) {
-    // let xhr = new XMLHttpRequest();
-    // xhr.open('GET', 'lazy.php?page=' + page, true);
-    // xhr.onload = function () {
-    //     if (xhr.status === 200) {
-    //         // let data = JSON.parse(`${xhr.responseText}`);
-    //         let data = xhr.responseText;
-    //         appendMusic(data);
-    //     }
-    // };
-    // xhr.send();
-
-    $.ajax({
-        type: "GET",
-        url: "lazy.php",
-        data: {
-            'page': page,
-        },
-        success: function (data) {
-            $(".album_list_wrapper album_list_wrapper_shop").append(data);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'lazy.php?page=' + page, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            // let data = JSON.parse(`${xhr.responseText}`);
+            let data = xhr.responseText;
+            appendMusic(data);
         }
-    });
+    };
+    xhr.send();
+
+    // $.ajax({
+    //     type: "GET",
+    //     url: "lazy.php",
+    //     data: {
+    //         'page': page,
+    //     },
+    //     success: function (data) {
+    //         $(".album_list_wrapper album_list_wrapper_shop").append(data);
+    //     }
+    // });
 }
 
 function appendMusic(music) {
     var element = document.getElementsByClassName("album_list_wrapper album_list_wrapper_shop");
+    console.log(element);
     element.innerHTML = music;
     $(".album_list_wrapper album_list_wrapper_shop").append(music);
 }
