57041913f914a8402dbc261ea92626d0fb7373f0
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-11-30 17:54:12 +0700

    Refactor music loading to use jQuery AJAX for improved performance and update music rendering in lazy.php for better structure

--- js/lazy.js
+++ js/lazy.js
@@ -11,47 +11,32 @@ window.addEventListener('scroll', function () {
 });
 
 function loadMoreMusic(page) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'lazy.php?page=' + page, true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            let data = JSON.parse(`${xhr.responseText}`);
-            appendMusic(data);
+    // let xhr = new XMLHttpRequest();
+    // xhr.open('GET', 'lazy.php?page=' + page, true);
+    // xhr.onload = function () {
+    //     if (xhr.status === 200) {
+    //         let data = JSON.parse(`${xhr.responseText}`);
+    //         appendMusic(data);
+    //     }
+    // };
+    // xhr.send();
+
+    $.ajax({
+        type: "GET",
+        url: "lazy.php",
+        data: {
+            'page': page,
+        },
+        success: function (data) {
+            $(".album_list_wrapper album_list_wrapper_shop").append(data);
         }
-    };
-    xhr.send();
+    });
 }
 
 function appendMusic(music) {
-    const musicList = document.querySelector('.album_inner_list_padding');
+    
 
     music.forEach(function (item) {
-        const musicItem = document.createElement('ul');
-        musicItem.classList.add('album_inner_list_padding');
-        musicItem.innerHTML = `
-        <li style="cursor: pointer;">
-            <a><span class="play_no">${item.number_music}</span>
-                <span class="play_hover" onclick="animatedPlayMusic(${item.number_music}, '${item.link_drive}', '${item.count_music}', '${item.id_music}', '${item.music_data}')">
-                    <i class="flaticon-play-button"></i>
-                </span></a>
-        </li>
-        <li class="song_title_width">
-            <div class="top_song_artist_wrapper">
-                <img src="${item.cover}" alt="img" class="cover_music">
-                <div class="top_song_artist_contnt">
-                    <h1><a class="title_music">${item.title}</a></h1>
-                    <p class="various_artist_text"><a class="artist_music">${item.artist}</a></p>
-                </div>
-            </div>
-        </li>
-        <li class="song_title_width"><a class="album_music">${item.album}</a></li>
-        <li class="text-center"><a class="time_music">${item.time}</a></li>
-        <li class="text-center favorite-text-center">
-            ${item.favorite ? `<i class="fas fa-heart" style="color: #1fd660;"></i>` : `<i class="far fa-heart" style="color: #fff;"></i>`}
-        </li>
-    `;
-
-        // Menambahkan elemen baru tepat setelah elemen terakhir
-        musicList.insertAdjacentElement('beforeend', musicItem);
+        
     });
 }
