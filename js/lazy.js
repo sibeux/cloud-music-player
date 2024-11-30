4972850b2ff464711aa7ab46da802198003fef41
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-11-30 20:51:05 +0700

    Comment out scroll event listener in lazy.js to disable automatic music loading on scroll

--- js/lazy.js
+++ js/lazy.js
@@ -1,13 +1,13 @@
 let currentPage = 1;
 
-window.addEventListener('scroll', function () {
-    const scrollHeight = document.documentElement.scrollHeight;
-    const scrollPosition = window.innerHeight + window.scrollY;
-    if (scrollHeight - scrollPosition <= 20) { // jika dekat dengan bawah
-        currentPage++;
-        loadMoreMusic(currentPage);
-    }
-});
+// window.addEventListener('scroll', function () {
+//     const scrollHeight = document.documentElement.scrollHeight;
+//     const scrollPosition = window.innerHeight + window.scrollY;
+//     if (scrollHeight - scrollPosition <= 20) { // jika dekat dengan bawah
+//         currentPage++;
+//         loadMoreMusic(currentPage);
+//     }
+// });
 
 function loadMoreMusic(page) {
     let xhr = new XMLHttpRequest();
