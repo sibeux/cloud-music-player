6f8c693d4e6cd349777784ff5467390b1b1493a6
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-11-30 20:39:29 +0700

    Prevent multiple fetch calls by adding a loading flag when scrolling to load more music

--- js/lazy.js
+++ js/lazy.js
@@ -1,18 +1,21 @@
 let currentPage = 1;
 
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollPosition = window.innerHeight + window.scrollY;
-    if (scrollHeight - scrollPosition <= 20) { // jika dekat dengan bawah
+    if (scrollHeight - scrollPosition <= 20 && !isFetching) {
+        isFetching = true; // Set flag untuk mencegah panggilan berulang
         currentPage++;
         console.log(currentPage);
-        loadMoreMusic(currentPage);
+        loadMoreMusic(currentPage).then(() => {
+            isFetching = false; // Reset setelah data selesai dimuat
+        });
     }
 });
 
 function loadMoreMusic(page) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'lazy.php?page=' + page, true);
+    xhr.open("GET", "lazy.php?page=" + page, true);
     xhr.onload = function () {
         if (xhr.status === 200) {
             // let data = JSON.parse(`${xhr.responseText}`);
