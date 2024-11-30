58a582264f5814396790b2eef1bf5bf83c5a4b23
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-11-30 18:02:22 +0700

    Refactor lazy.php for improved code readability and structure

--- lazy.php
+++ lazy.php
@@ -1,7 +1,7 @@
 <?php
 include "./database/db.php";
 // load_more_music.php
-$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 $limit = 20;
 $offset = ($page - 1) * $limit;
 
@@ -17,7 +17,7 @@ $number_music = $offset + 1;
 
 $music_data = [];
 while ($array_data_music = mysqli_fetch_array($result_music)) {
-// Proses data seperti yang Anda lakukan sebelumnya
+    // Proses data seperti yang Anda lakukan sebelumnya
     $link_drive = checkUrlFromDrive($array_data_music['link_gdrive'], $api_key);
     $current_number_music = $number_music;
     $id_music = $array_data_music['id_music'];
@@ -29,18 +29,18 @@ while ($array_data_music = mysqli_fetch_array($result_music)) {
     $time = $array_data_music['time'];
     $date_added = $array_data_music['date_added'];
     $cover = checkUrlFromDrive($array_data_music['cover'], $api_key);
-    
-$data = [
-'artist' => addslashes($array_data_music['artist']),
-'category' => $array_data_music['category'],
-'cover' => $array_data_music['cover'],
-'date_added' => $array_data_music['date_added'],
-'favorite' => $array_data_music['favorite'],
-'id_music' => $array_data_music['id_music'],
-'link_gdrive' => $array_data_music['link_gdrive'],
-'time' => $array_data_music['time'],
-'title' => addslashes($array_data_music['title']),
-];
+
+    $data = [
+        'artist' => addslashes($array_data_music['artist']),
+        'category' => $array_data_music['category'],
+        'cover' => $array_data_music['cover'],
+        'date_added' => $array_data_music['date_added'],
+        'favorite' => $array_data_music['favorite'],
+        'id_music' => $array_data_music['id_music'],
+        'link_gdrive' => $array_data_music['link_gdrive'],
+        'time' => $array_data_music['time'],
+        'title' => addslashes($array_data_music['title']),
+    ];
 
     $music_data = htmlspecialchars(
         json_encode(
@@ -94,8 +94,8 @@ $data = [
 }
 
 // header('Content-Type: application/json');
-// echo
-// json_encode(
-// $music_data,
-// JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
-// );
+echo
+    json_encode(
+        $music_data,
+        JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+    );
