dfed525557d2ddaab1dd56d65345dae11a9b2eb9
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-11-30 18:06:52 +0700

    Refactor music loading in lazy.js to use XMLHttpRequest and streamline music data processing in lazy.php for improved performance and cleaner JSON response

--- js/lazy.js
+++ js/lazy.js
@@ -11,26 +11,26 @@ window.addEventListener('scroll', function () {
 });
 
 function loadMoreMusic(page) {
-    // let xhr = new XMLHttpRequest();
-    // xhr.open('GET', 'lazy.php?page=' + page, true);
-    // xhr.onload = function () {
-    //     if (xhr.status === 200) {
-    //         let data = JSON.parse(`${xhr.responseText}`);
-    //         appendMusic(data);
-    //     }
-    // };
-    // xhr.send();
-
-    $.ajax({
-        type: "GET",
-        url: "lazy.php",
-        data: {
-            'page': page,
-        },
-        success: function (data) {
-            $(".album_list_wrapper album_list_wrapper_shop").append(data);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'lazy.php?page=' + page, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            let data = JSON.parse(`${xhr.responseText}`);
+            appendMusic(data);
         }
-    });
+    };
+    xhr.send();
+
+    // $.ajax({
+    //     type: "GET",
+    //     url: "lazy.php",
+    //     data: {
+    //         'page': page,
+    //     },
+    //     success: function (data) {
+    //         $(".album_list_wrapper album_list_wrapper_shop").append(data);
+    //     }
+    // });
 }
 
 function appendMusic(music) {
