8ce4a64e03962d59a330e3a0def85493184c63c7
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-02-19 07:46:07 +0700

    upload music

--- database/mobile-music-player/api/playlist.php
+++ database/mobile-music-player/api/playlist.php
@@ -33,8 +33,8 @@ if (isset($_GET['type']) && isset($_GET['uid'])) {
 
     if ($type == 'album') {
         $sql = "SELECT * FROM music 
-        join playlist on music.album like CONCAT('%', playlist.name, '%') 
-        where playlist.uid = '$uid'
+        JOIN playlist ON music.album LIKE CONCAT('%', TRIM(playlist.name), '%') 
+        WHERE playlist.uid = '$uid'
         ORDER BY title ASC";
     }
 
