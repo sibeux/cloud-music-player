543793d07cb7fde5b9c57470fbd4dedc0924d2d6
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-02-28 07:03:13 +0700

    feat: add read_codec.php to retrieve codec information from media files

---
+++ database/mobile-music-player/api/read_codec.php
@@ -0,0 +1,10 @@
+<?php
+$file = $_POST['url'];
+
+// Jalankan ffprobe untuk mendapatkan codec info dalam format JSON
+$command = "ffprobe -i \"$file\" -show_streams -show_format -print_format json 2>&1";
+// $metadata = shell_exec("ffmpeg -i $file -f ffmetadata -");
+$codec = shell_exec($command);
+
+// echo "<pre>$metadata</pre>";
+echo "<pre>$codec</pre>";
