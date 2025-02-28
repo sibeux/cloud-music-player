dbc6a2ebefbbcb7f9395013078c8eb066993c8fd
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-02-28 07:49:34 +0700

    upload music

--- database/mobile-music-player/api/read_codec.php
+++ database/mobile-music-player/api/read_codec.php
@@ -1,10 +1,5 @@
 <?php
-$file = $_POST['url'];
-
-// Jalankan ffprobe untuk mendapatkan codec info dalam format JSON
-$command = "ffprobe -i \"$file\" -show_streams -show_format -print_format json 2>&1";
-// $metadata = shell_exec("ffmpeg -i $file -f ffmetadata -");
-$codec = shell_exec($command);
-
-// echo "<pre>$metadata</pre>";
-echo $file;
+echo "<pre>";
+print_r(ini_get('disable_functions'));
+echo "</pre>";
+?>
