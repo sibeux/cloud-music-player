9e85f426e18d260ab520a77389f37426064f76d2
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-07-05 20:00:34 +0700

    Add comment for JSON response section in playlist API

--- database/mobile-music-player/api/playlist.php
+++ database/mobile-music-player/api/playlist.php
@@ -79,6 +79,7 @@ if (isset($_GET['recents_music'])) {
     $sql = "SELECT * FROM recents_music join music on music.id_music = recents_music.uid_music ORDER BY played_at DESC";
 }
 
+// Response json
 $result = $db->query($sql);
 if (!$result) {
     http_response_code(500);
