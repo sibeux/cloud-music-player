17e97d0cd1f20fcc747ab7f426557bd0ae514f6f
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-08-19 04:19:50 +0700

    test path lagi

--- api/cache_to_server.php
+++ api/cache_to_server.php
@@ -1,6 +1,6 @@
 <?php
 
-include './../database/mobile-music-player/api/connection.php';
+require_once __DIR__ . '/../database/mobile-music-player/api/connection.php';
 
 function cacheMusicToServer($fileId, $accessToken, $musicId){
 	// --- Unduh file dari Google Drive dan simpan ke cache ---
