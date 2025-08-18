657182552e28ab9bf43fb7de55fa3c79a7d95934
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-08-18 21:55:23 +0700

    test sql insert metadata

--- database/mobile-music-player/api/recents_music.php
+++ database/mobile-music-player/api/recents_music.php
@@ -3,14 +3,21 @@
 include './connection.php';
 
 $sql = "";
+$metadata_sql = "";
 $delete = "";
 
-if (isset($_GET['_id'])) {
+if (isset($_POST['music_id'])) {
 
-    $_id = $_GET['_id'];
+    $_id = $_POST['music_id'];
+    $codec_name = $_POST['codec_name'];
+    $quality = $_POST['quality'];
+    $bits_per_raw_sample = $_POST['bits_per_raw_sample'];
+    $sample_rate = $_POST['sample_rate'];
+    $bit_rate = $_POST['bit_rate'];
 
     // kalau mau tidak ada duplikasi musik di recents, maka uid_music harus dijadikan unique/foreign key
     $sql = "INSERT INTO recents_music (uid_recents, uid_music, played_at) values (NULL, '$_id', NOW())";
+    $metadata_sql = "INSERT INTO `metadata_music` (`metadata_id_music`, `codec_name`, `music_quality`, `sample_rate`, `bit_rate`, `bits_per_raw_sample`) VALUES ($_id, $codec_name, $quality, $bits_per_raw_sample, $sample_rate, $bit_rate);"
     $delete = "DELETE FROM recents_music
 WHERE uid_recents NOT IN (
     SELECT uid_recents
@@ -25,6 +32,7 @@ WHERE uid_recents NOT IN (
 
 // Query to retrieve data from MySQL
 $result = $db->query($sql);
+$result = $db->query($metadata_sql);
 $result = $db->query($delete);
 
 // Check if the query was successful
