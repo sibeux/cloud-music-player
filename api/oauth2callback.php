8d32bd3e5176097bab50cf59a6509b1a8dec72f6
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-08-27 23:34:23 +0700

    34

--- api/oauth2callback.php
+++ api/oauth2callback.php
@@ -7,7 +7,7 @@ include __DIR__ . '/google-oauth-config.php';
 // Ambil code dari query parameter
 $code = $_GET['code'] ?? null;
 // GANTI EMAIL INI SETIAP MAU BUAT OAUTH KEY BARU
-$email = "wahabinasrul@gmail.com";
+$email = "yuukisibeux@gmail.com";
 
 if (!$code) {
     die("No authorization code received.");
